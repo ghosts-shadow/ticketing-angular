8309d462d6fa005239c5195c06f950559d37718b
sneden <dinizsneden2@gmail.com>
2023-10-02 17:05:00 +0400

    firebase link test

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,11 @@
+export const defaultEnvironment = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  production: false
+};
